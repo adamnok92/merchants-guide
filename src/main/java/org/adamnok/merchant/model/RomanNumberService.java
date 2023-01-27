13f0a26da4a2cb76fc6f33f186b8f8e87515d214
Adam Nok <nokadams@gmail.com>
2023-01-27 13:05:16 +0100

    Implement RomanNumber service

--- src/main/java/org/adamnok/merchant/model/RomanNumberService.java
+++ src/main/java/org/adamnok/merchant/model/RomanNumberService.java
@@ -1,16 +1,24 @@
 package org.adamnok.merchant.model;
 
+import com.github.fracpete.romannumerals4j.RomanNumeralFormat;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.util.Optional;
 
 @Service
 public class RomanNumberService {
+    private final RomanNumeralFormat romanNumeralFormat = new RomanNumeralFormat();
+
     public Optional<Integer> fromRoman(String value) {
-        throw new UnsupportedOperationException();
+        try {
+            return Optional.of(romanNumeralFormat.parse(value).intValue());
+        } catch (ParseException e) {
+            return Optional.empty();
+        }
     }
 
     public String toRoman(int value) {
-        throw new UnsupportedOperationException();
+        return romanNumeralFormat.format(value);
     }
 }
