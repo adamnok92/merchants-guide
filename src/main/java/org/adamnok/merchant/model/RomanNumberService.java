0e9c73f47a7f247d4137c8cd3768659cb5d838cd
Adam Nok <nokadams@gmail.com>
2023-01-27 13:39:10 +0100

    Implement repository foreign numbers

--- src/main/java/org/adamnok/merchant/model/RomanNumberService.java
+++ src/main/java/org/adamnok/merchant/model/RomanNumberService.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class RomanNumberService {
+    public static Set<Character> NUMBERS = Set.of('I', 'V', 'X', 'L', 'C', 'M');
     private final RomanNumeralFormat romanNumeralFormat = new RomanNumeralFormat();
 
     public Optional<Integer> fromRoman(String value) {
--- src/main/java/org/adamnok/merchant/repositories/ForeignNumberRepository.java
+++ src/main/java/org/adamnok/merchant/repositories/ForeignNumberRepository.java
@@ -1,21 +1,42 @@
 package org.adamnok.merchant.repositories;
 
+import org.adamnok.merchant.exceptions.InvalidForeignNumberException;
+import org.adamnok.merchant.exceptions.InvalidRomanNumberException;
+import org.adamnok.merchant.model.RomanNumberService;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Repository
-public class ForeignNumberRepository {
-    public Set<String> getAllForeignNumbers(){
-        throw new UnsupportedOperationException();
+public final class ForeignNumberRepository {
+    private final Map<String, Character> numbers = new HashMap<>();
+
+    public Set<String> getAllForeignNumbers() {
+        return numbers.keySet();
     }
 
     public void register(ForeignNumber number) {
-        throw new UnsupportedOperationException();
+        if (!RomanNumberService.NUMBERS.contains(number.romanNumber())) {
+            throw new InvalidRomanNumberException(number.romanNumber());
+        }
+        numbers.put(number.name(), number.romanNumber());
+    }
+
+    private char getRomanNumber(String foreignNumber) {
+        if (!numbers.containsKey(foreignNumber)) {
+            throw new InvalidForeignNumberException(foreignNumber);
+        }
+        return numbers.get(foreignNumber);
     }
 
     public String getRomanNumber(List<String> foreignNumbers) {
-        throw new UnsupportedOperationException();
+        return foreignNumbers.stream()
+            .map(this::getRomanNumber)
+            .map(Object::toString)
+            .collect(Collectors.joining(""));
     }
 }
